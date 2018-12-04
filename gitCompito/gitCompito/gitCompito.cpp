ec2023590703b667e18cb5dc13a2a04bd5f11716
whoselore <alampo.lorenzo@gmail.com>
2018-12-04 10:36:13 +0100

    aggiunto indice i

--- gitCompito/gitCompito/gitCompito.cpp
+++ gitCompito/gitCompito/gitCompito.cpp
@@ -8,15 +8,6 @@ int main()
 
 	int i = 0;
 
-	for (int i = 0; i < 10; i++)
-	{
-		conta++;
-<<<<<<< HEAD
-		cout << "cout variabile"<<conta << endl;
-=======
-		cout << "cout della variabile"<<conta << endl;
->>>>>>> BUG_FIX
-	}
 
 	system("pause");
 
