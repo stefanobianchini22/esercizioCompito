24e952d59d29ecff279731a16fee934e365c8f9b
whoselore <alampo.lorenzo@gmail.com>
2018-12-04 10:33:16 +0100

    aggiunta variabile conta

---
+++ gitCompito/gitCompito/gitCompito.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int conta = 0;
+
+	int i = 0;
+
+	for (int i = 0; i < 10; i++)
+	{
+		conta++;
+<<<<<<< HEAD
+		cout << "cout variabile"<<conta << endl;
+=======
+		cout << "cout della variabile"<<conta << endl;
+>>>>>>> BUG_FIX
+	}
+
+	system("pause");
+
+}
